8f398b1bfb483df73d17c83e46f5fa1792aba9b7
Fajrin Mahyuddin <fajrin.telkom12@gmail.com>
2021-08-31 19:17:43 +0800

    fix asyncronous ssr recoil request

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -31,7 +31,6 @@ const MyApp = ({ Component, pageProps }) => {
 			</Head>
 			<RecoilRoot>
 				<ErrorBoundary>
-
 					<AppProvider>
 						<QueryClientProvider client={queryClient}>
 							<Component {...pageProps} pageLoading={loading} />
@@ -93,7 +92,7 @@ class ErrorBoundary extends React.Component {
 	render() {
 		if (this.state.hasError) {
 			// You can render any custom fallback UI
-			return <h1>Something went wrong.</h1>;
+			return <h3>Something went wrong.</h3>;
 		}
 
 		return this.props.children;
--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,20 +1,22 @@
 import nookies from "nookies";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StandartLayout } from "layout";
 import { useRouter } from "next/router";
 import admin from "utils/firebase-admin";
 import { logout } from "utils/firebase-auth";
 import { redirectTo, useAppContext } from "utils/auth";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { filterState, funFilter, queryState, stateStatistics } from "utils/recoil-state";
 
 const ProfilePage = () => {
 	const state_global = useRecoilValue(funFilter);
 	const statistics = useRecoilValue(stateStatistics);
-	const fetchingData = useRecoilValue(queryState);
+	const query = useRecoilValueLoadable(queryState);
 	const [filter, setFilter] = useRecoilState(filterState)
-	const router = useRouter()
-	const { authUser, loading } = useAppContext()
+	const router = useRouter();
+	const [isMounted, setMounted] = useState(false);
+	const { authUser, loading } = useAppContext();
+
 	const handleLogout = async () => {
 		try {
 			await logout();
@@ -23,33 +25,35 @@ const ProfilePage = () => {
 			throw Error("error logout")
 		}
 	}
-	console.log("state global", fetchingData);
-	if (!fetchingData) {
-		return <h1>wait a second...</h1>
-	} else {
 
-		return (
-			<StandartLayout>
-				<StandartLayout.Content>
-					<button onClick={handleLogout}>
-						logout
-					</button>
-					{/* total : {state_global.leng}
+	// useEffect(() => {
+	// 	setMounted(true)
+	// 	return () => setMounted(false)
+	// }, [])
+
+	console.log("state global", query);
+	if (query.state === 'loading') return <h1> loading ...</h1>
+	return (
+		<StandartLayout>
+			<StandartLayout.Content>
+				<button onClick={handleLogout}>
+					logout
+				</button>
+				{/* total : {state_global.leng}
 				<select value={filter} name="bla" id="bla" onChange={(e) => setFilter(e.target.value)}>
 					<option value="">pilih</option>
 					<option value="one">one</option>
 					<option value="two">two</option>
 				</select>
+			*/}
 				<ul>
-					{state_global?.list.map((item, i) => {
-						return <li key={i}>{item.foo}</li>
+					{query?.contents?.map((item, i) => {
+						return <li key={i}>{item.title}</li>
 					})}
-				</ul> */}
-				</StandartLayout.Content>
-			</StandartLayout>
-		)
-	}
-
+				</ul>
+			</StandartLayout.Content>
+		</StandartLayout>
+	)
 }
 
 export const getServerSideProps = async (ctx) => {
